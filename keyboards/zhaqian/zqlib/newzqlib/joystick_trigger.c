bfa6a6f54f6db1d68e2769a266f1855b6d1773c6
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2022-02-03 14:55:19 +0800

    add joystick trigger driver
     - add joystick trigger driver(only support two axes)
     -  Joystick virtual button and no hid device

---
+++ keyboards/zhaqian/zqlib/newzqlib/joystick_trigger.c
@@ -0,0 +1,86 @@
+/* Copyright 2022 zhaqian
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "joystick_trigger.h"
+#include "analog.h"
+
+#ifndef JOYSTICK_ADC_RESOLUTION
+#    define JOYSTICK_ADC_RESOLUTION 10
+#elif JOYSTICK_ADC_RESOLUTION < 8 || JOYSTICK_ADC_RESOLUTION > 16
+#    error JOYSTICK_ADC_RESOLUTION must be between 8 and 16
+#endif
+
+#define JOYSTICK_RESOLUTION ((1L << (JOYSTICK_ADC_RESOLUTION - 1)) - 1)
+
+#ifndef JOYSTICK_THRESHOLD
+#define JOYSTICK_THRESHOLD (JOYSTICK_RESOLUTION / 2)
+#endif
+
+#if !defined(JOYSTICK_AXES_X_PIN) || !defined(JOYSTICK_AXES_Y_PIN)
+#    error "No encoder pads defined by ENCODERS_PAD_A and ENCODERS_PAD_B"
+#endif
+
+static pin_t joystick_axes_x_pin[] = JOYSTICK_AXES_X_PIN;
+static pin_t joystick_axes_y_pin[] = JOYSTICK_AXES_Y_PIN;
+
+#define NUMBER_OF_JOYSTICKS (sizeof(joystick_axes_x_pin) / sizeof(pin_t))
+
+static joystick_axes_state_t joystick_axes_state[NUMBER_OF_JOYSTICKS];
+
+__attribute__((weak)) bool joystick_update_user(uint8_t index, joystick_axes_state_t axes_state) {
+    return true;
+}
+
+__attribute__((weak)) bool joystick_update_kb(uint8_t index, joystick_axes_state_t axes_state) {
+    return joystick_update_user(index, axes_state);
+}
+
+void joystick_init(void) {
+    joystick_axes_state_t init_state;
+    init_state.joystick_axes_trigger_state.raw = 0;
+    for (uint8_t i = 0; i < NUMBER_OF_JOYSTICKS; i ++) {
+        init_state.joystick_axes_x_value = analogReadPin(joystick_axes_x_pin[i]);
+        init_state.joystick_axes_y_value = analogReadPin(joystick_axes_y_pin[i]);
+        joystick_axes_state[i] = init_state;
+    }
+}
+
+static void joystick_update(uint8_t index) {
+    bool changed = false;
+    joystick_axes_trigger_t current_state;
+    int16_t axes_x_buf = joystick_axes_state[index].joystick_axes_x_value - JOYSTICK_RESOLUTION;
+    int16_t axes_y_buf = joystick_axes_state[index].joystick_axes_y_value - JOYSTICK_RESOLUTION;
+    current_state.positive_x = axes_x_buf > JOYSTICK_THRESHOLD ? 1 : 0;
+    current_state.negative_x = axes_x_buf < -JOYSTICK_THRESHOLD ? 1 : 0;
+    current_state.positive_y = axes_y_buf > JOYSTICK_THRESHOLD ? 1 : 0;
+    current_state.negative_y = axes_y_buf < -JOYSTICK_THRESHOLD ? 1 : 0;
+    if (current_state.raw != joystick_axes_state[index].joystick_axes_trigger_state.raw) {
+        joystick_axes_state[index].joystick_axes_trigger_state = current_state;
+        changed != joystick_update_kb(index, current_state);
+    }
+    return changed;
+}
+
+bool joystick_task(void) {
+    bool changed = false;
+    for (uint8_t i = 0; i < NUMBER_OF_JOYSTICKS; i++) {
+        joystick_axes_state[i].joystick_axes_x_value = analogReadPin(joystick_axes_x_pin[i]);
+        joystick_axes_state[i].joystick_axes_y_value = analogReadPin(joystick_axes_y_pin[i]);
+        changed |= joystick_update(i);
+    }
+    return changed;
+}
+
