77d83977f832f901507125f220ba8a13f33642b6
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2022-02-26 20:42:03 +0800

    add Build Option

--- builddefs/common_features.mk
+++ builddefs/common_features.mk
@@ -814,3 +814,71 @@ ifeq ($(strip $(BLUETOOTH_ENABLE)), yes)
         QUANTUM_LIB_SRC += uart.c
     endif
 endif
+
+ifeq ($(strip $(ALT_TAB_MARCO_ENABLE)), yes)
+    SRC += $(QUANTUM_DIR)/alt_tab_marco.c
+    OPT_DEFS += -DALT_TAB_MARCO_ENABLE
+endif
+
+ifeq ($(strip $(ENCODER_TRIGGER_ENABLE)), yes)
+    ifeq ($(strip $(ENCODER_ENABLE)), no)
+        $(error ENCODER_TRIGGER_ENABLE requires ENCODER_ENABLE, either disable ENCODER_TRIGGER explicitly or enable ENCODER)
+    endif
+    SRC += $(QUANTUM_DIR)/encoder_trigger.c
+    OPT_DEFS += -DENCODER_TRIGGER_ENABLE
+endif
+
+ifeq ($(strip $(JOYSTICK_TRIGGER_ENABLE)), yes)
+    SRC += $(QUANTUM_DIR)/joystick_trigger.c
+    OPT_DEFS += -DJOYSTICK_TRIGGER_ENABLE
+endif
+
+ifeq ($(strip $(OPENRGB_ENABLE)), yes)
+	ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
+        $(error OPENRGB_ENABLE requires RGB_MATRIX_ENABLE, either disable OPENRGB_ENABLE explicitly or enable RGB_MATRIX)
+    endif
+    RAW_ENABLE := yes
+    SRC += $(QUANTUM_DIR)/rgb_matrix/openrgb.c
+    OPT_DEFS += -DOPENRGB_ENABLE
+	OPT_DEFS += -DOPENRGB_PROTOCOL_ENABLE
+endif
+
+ifeq ($(strip $(RADIAL_CONTROLLER_ENABLE)), yes)
+    ifeq ($(strip $(EXTRAKEY_ENABLE)), no)
+        $(error RADIAL_CONTROLLER_ENABLE requires EXTRAKEY_ENABLE, either disable RADIAL_CONTROLLER explicitly or enable EXTRAKEY)
+    endif
+    SRC += $(QUANTUM_DIR)/radial_controller.c
+    OPT_DEFS += -DRADIAL_CONTROLLER_ENABLE
+endif
+
+ifeq ($(strip $(RGB_INDICATORS_ENABLE)), yes)
+	ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
+        $(error RGB_INDICATORS_ENABLE requires RGB_MATRIX_ENABLE, either disable RGB_INDICATORS explicitly or enable RGB_MATRIX)
+    endif
+    SRC += $(QUANTUM_DIR)/rgb_matrix/rgb_indicators.c
+    OPT_DEFS += -DRGB_INDICATORS_ENABLE
+endif
+
+ifeq ($(strip $(RGB_MATRIX_CONTROL_ENABLE)), yes)
+	ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
+        $(error RGB_MATRIX_CONTROL_ENABLE requires RGB_MATRIX_ENABLE, either disable RGB_MATRIX_CONTROL explicitly or enable RGB_MATRIX)
+    endif
+    SRC += $(QUANTUM_DIR)/rgb_matrix/rgb_matrix_control.c
+	OPT_DEFS += -DRGB_MATRIX_CONTROL_ENABLE
+endif
+
+ifeq ($(strip $(UNDERGLOW_RGB_MATRIX_ENABLE)), yes)
+	ifeq ($(strip $(RGB_MATRIX_ENABLE)), no)
+        $(error UNDERGLOW_RGB_MATRIX_ENABLE requires RGB_MATRIX_ENABLE, either disable UNDERGLOW_RGB_MATRIX explicitly or enable RGB_MATRIX)
+    endif
+    SRC += $(QUANTUM_DIR)/rgb_matrix/underglow_rgb_matrix.c
+    OPT_DEFS += -DUNDERGLOW_RGB_MATRIX_ENABLE
+endif
+
+ifeq ($(strip $(VIA_CUSTOM_KEYCODE_ENABLE)), yes)
+	ifeq ($(strip $(VIA_ENABLE)), no)
+        $(error VIA_CUSTOM_KEYCODE_ENABLE requires VIA_ENABLE, either disable VIA_CUSTOM_KEYCODE explicitly or enable VIA)
+    endif
+    SRC += $(QUANTUM_DIR)/via_custom_keycode.c
+	OPT_DEFS += -DVIA_CUSTOM_KEYCODE_ENABLE
+endif
