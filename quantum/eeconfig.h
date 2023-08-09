727c2d01d36ca47f1aa1912f99064ee9818f86ea
zhaqian <80608733+zhaqian12@users.noreply.github.com>
2023-08-09 22:06:37 +0800

    fix eeconfig bug

--- quantum/eeconfig.h
+++ quantum/eeconfig.h
@@ -66,6 +66,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EECONFIG_ZHAQIAN_SIZE 60
 #endif
 
+#ifndef EECONFIG_ZHAQIAN_SIZE
+#    define EECONFIG_ZHAQIAN_SIZE 0
+#endif
+
 // Size of EEPROM dedicated to keyboard- and user-specific data
 #ifndef EECONFIG_KB_DATA_SIZE
 #    define EECONFIG_KB_DATA_SIZE 0
